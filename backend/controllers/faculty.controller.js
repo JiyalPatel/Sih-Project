b5e2e1f89bbe7f23e9f4b4338ce9f88eee4babc7
JiyalPatel <jiyalpatel10@gmail.com>
2025-09-24 17:24:41 +0530

    updated faculty controller

--- backend/controllers/faculty.controller.js
+++ backend/controllers/faculty.controller.js
@@ -1,6 +1,6 @@
 const Faculty = require("../models/faculty.model");
 const User = require("../models/user.models");
-
+const Department = require("../models/department.model");
 
 // Create Faculty with User account
 const createFaculty = async (req, res) => {
@@ -12,13 +12,18 @@ const createFaculty = async (req, res) => {
             specialization,
             preferred_days,
             preferred_slots,
+            departmentId, // Added departmentId to the request body
         } = req.body;
 
         const userExists = await User.findOne({ email });
-        if (userExists)
+        if (userExists) {
             return res.status(400).json({ msg: "Email already exists" });
+        }
 
-        
+        const department = await Department.findById(departmentId);
+        if (!department) {
+            return res.status(404).json({ msg: "Department not found" });
+        }
 
         // Create User with role "faculty"
         const user = await User.create({
@@ -28,13 +33,18 @@ const createFaculty = async (req, res) => {
             role: "faculty",
         });
 
-        // Create Faculty linked to User
+        // Create Faculty linked to User and Department
         const faculty = await Faculty.create({
             fac_acc: user._id,
+            department: department._id, // Link to department
             specialization: specialization || [],
             preferred_days: preferred_days || [],
             preferred_slots: preferred_slots || [],
         });
+
+        // Add faculty to department's faculties array
+        department.faculties.push(faculty._id);
+        await department.save();
 
         res.status(201).json({
             msg: "Faculty and account created successfully",
@@ -49,10 +59,9 @@ const createFaculty = async (req, res) => {
 // Get all Faculty
 const getFaculties = async (req, res) => {
     try {
-        const faculties = await Faculty.find().populate(
-            "fac_acc",
-            "name email"
-        );
+        const faculties = await Faculty.find()
+            .populate("fac_acc", "name email")
+            .populate("department", "name"); // Populate department name
         res.json(faculties);
     } catch (err) {
         res.status(500).json({ msg: err.message });
@@ -62,10 +71,9 @@ const getFaculties = async (req, res) => {
 // Get single Faculty by ID
 const getFacultyById = async (req, res) => {
     try {
-        const faculty = await Faculty.findById(req.params.id).populate(
-            "fac_acc",
-            "name email"
-        );
+        const faculty = await Faculty.findById(req.params.id)
+            .populate("fac_acc", "name email")
+            .populate("department", "name"); // Populate department name
         if (!faculty) return res.status(404).json({ msg: "Faculty not found" });
         res.json(faculty);
     } catch (err) {
@@ -88,13 +96,16 @@ const updateFaculty = async (req, res) => {
             specialization,
             preferred_days,
             preferred_slots,
+            departmentId,
         } = req.body;
+
+        const oldDepartmentId = faculty.department;
 
         // Update linked User
         const user = await User.findById(faculty.fac_acc._id);
         if (name) user.name = name;
         if (email) user.email = email;
-        if (password) user.password =password;
+        if (password) user.password = password;
 
         await user.save();
 
@@ -102,16 +113,28 @@ const updateFaculty = async (req, res) => {
         if (specialization) faculty.specialization = specialization;
         if (preferred_days) faculty.preferred_days = preferred_days;
         if (preferred_slots) faculty.preferred_slots = preferred_slots;
+        if (departmentId) faculty.department = departmentId;
 
         await faculty.save();
 
-        res.json({
-            _id: faculty._id,
-            fac_acc: { _id: user._id, name: user.name, email: user.email },
-            specialization: faculty.specialization,
-            preferred_days: faculty.preferred_days,
-            preferred_slots: faculty.preferred_slots,
-        });
+        // Update department's faculty list if department changed
+        if (
+            departmentId &&
+            departmentId.toString() !== oldDepartmentId?.toString()
+        ) {
+            await Department.findByIdAndUpdate(oldDepartmentId, {
+                $pull: { faculties: faculty._id },
+            });
+            await Department.findByIdAndUpdate(departmentId, {
+                $addToSet: { faculties: faculty._id },
+            });
+        }
+
+        const updatedFaculty = await Faculty.findById(faculty._id)
+            .populate("fac_acc", "name email")
+            .populate("department", "name");
+
+        res.json(updatedFaculty);
     } catch (err) {
         res.status(500).json({ msg: err.message });
     }
@@ -122,6 +145,11 @@ const deleteFaculty = async (req, res) => {
     try {
         const faculty = await Faculty.findById(req.params.id);
         if (!faculty) return res.status(404).json({ msg: "Faculty not found" });
+
+        // Remove faculty from department
+        await Department.findByIdAndUpdate(faculty.department, {
+            $pull: { faculties: faculty._id },
+        });
 
         await User.findByIdAndDelete(faculty.fac_acc);
         await faculty.deleteOne();
